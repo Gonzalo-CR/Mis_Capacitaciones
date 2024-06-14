fc04fe026949700874240f4f2ab4f9386ddfdf32
Gonzalo_CR <161965777+Gonzalo-CR@users.noreply.github.com>
2024-06-14 20:07:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,21 +5,13 @@ const path = require('path');
 const PORT = 5500;
 const HOST = '192.168.0.145';
 
-// Define la carpeta de archivos estáticos (la carpeta raíz del proyecto)
-app.use('/images/', express.static(path.join(__dirname, '')));
-/*
+// Define la carpeta de archivos estáticos
+app.use(express.static(path.join(__dirname)));
+
 // Ruta para el archivo index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-// Ruta para el archivo script.js
-app.get('/script.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'script.js'));
-});
-*/
-// Ruta para la carpeta de imágenes
-app.use('', express.static(path.join(__dirname, '')));
 
 // Escucha en el puerto y la dirección especificados
 app.listen(PORT, HOST, () => {
