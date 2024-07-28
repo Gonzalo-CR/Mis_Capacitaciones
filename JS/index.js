0c7d29e4d6351e23bef0808c873731fd86c35a93
Gonzalo_CR <161965777+Gonzalo-CR@users.noreply.github.com>
2024-07-28 19:58:27 -0300

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let images = [];
     const misDiplomas = 'images/';
 
-    fetch('../JSON/index.json')
+    fetch('JSON/index.json')
         .then(response => response.json())
         .then(imageFiles => {
             images = imageFiles;
